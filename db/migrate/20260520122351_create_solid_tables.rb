724c952abf9d27c299c7cc27c82bafe39bf23b23
Sumit Maurya <mauryasumit222@gmail.com>
2026-05-20 17:55:04 +0530

    Add solid queue cache cable migrations

---
+++ db/migrate/20260520122351_create_solid_tables.rb
@@ -0,0 +1,151 @@
+class CreateSolidTables < ActiveRecord::Migration[8.1]
+    create_table :solid_cache_entries, force: :cascade do |t|
+      t.binary  :key,        null: false, limit: 1024
+      t.binary  :value,      null: false, limit: 536870912
+      t.datetime :created_at, null: false
+      t.bigint  :key_hash,   null: false
+      t.integer :byte_size,  null: false
+      t.index [:key_hash], unique: true
+      t.index [:byte_size]
+      t.index [:created_at]
+    end
+
+    # Solid Cable
+    create_table :solid_cable_messages, force: :cascade do |t|
+      t.text    :channel,    null: false
+      t.text    :payload,    null: false
+      t.datetime :created_at, null: false
+      t.index [:channel]
+      t.index [:created_at]
+    end
+
+    # Solid Queue
+    create_table :solid_queue_jobs, force: :cascade do |t|
+      t.string   :queue_name,       null: false
+      t.string   :class_name,       null: false
+      t.text     :arguments
+      t.integer  :priority,         default: 0, null: false
+      t.string   :active_job_id
+      t.datetime :scheduled_at
+      t.datetime :finished_at
+      t.string   :concurrency_key
+      t.datetime :created_at,       null: false
+      t.datetime :updated_at,       null: false
+      t.index [:active_job_id]
+      t.index [:class_name]
+      t.index [:finished_at]
+      t.index [:queue_name, :finished_at]
+      t.index [:scheduled_at, :finished_at]
+    end
+
+    create_table :solid_queue_scheduled_executions, force: :cascade do |t|
+      t.bigint   :job_id,       null: false
+      t.string   :queue_name,   null: false
+      t.integer  :priority,     default: 0, null: false
+      t.datetime :scheduled_at, null: false
+      t.datetime :created_at,   null: false
+      t.index [:job_id], unique: true
+      t.index [:scheduled_at, :priority, :job_id]
+    end
+
+    create_table :solid_queue_ready_executions, force: :cascade do |t|
+      t.bigint   :job_id,     null: false
+      t.string   :queue_name, null: false
+      t.integer  :priority,   default: 0, null: false
+      t.datetime :created_at, null: false
+      t.index [:job_id], unique: true
+      t.index [:priority, :job_id]
+      t.index [:queue_name, :priority, :job_id]
+    end
+
+    create_table :solid_queue_claimed_executions, force: :cascade do |t|
+      t.bigint   :job_id,     null: false
+      t.bigint   :process_id
+      t.datetime :created_at, null: false
+      t.index [:job_id], unique: true
+      t.index [:process_id, :job_id]
+    end
+
+    create_table :solid_queue_blocked_executions, force: :cascade do |t|
+      t.bigint   :job_id,          null: false
+      t.string   :queue_name,      null: false
+      t.integer  :priority,        default: 0, null: false
+      t.string   :concurrency_key, null: false
+      t.datetime :expires_at,      null: false
+      t.datetime :created_at,      null: false
+      t.index [:job_id], unique: true
+      t.index [:expires_at, :concurrency_key]
+      t.index [:concurrency_key, :priority, :job_id]
+    end
+
+    create_table :solid_queue_failed_executions, force: :cascade do |t|
+      t.bigint   :job_id,     null: false
+      t.text     :error
+      t.datetime :created_at, null: false
+      t.index [:job_id], unique: true
+    end
+
+    create_table :solid_queue_pauses, force: :cascade do |t|
+      t.string   :queue_name, null: false
+      t.datetime :created_at, null: false
+      t.index [:queue_name], unique: true
+    end
+
+    create_table :solid_queue_semaphores, force: :cascade do |t|
+      t.string   :key,        null: false
+      t.integer  :value,      default: 1, null: false
+      t.datetime :expires_at, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+      t.index [:expires_at]
+      t.index [:key, :value]
+      t.index [:key], unique: true
+    end
+
+    create_table :solid_queue_processes, force: :cascade do |t|
+      t.string   :kind,         null: false
+      t.datetime :last_heartbeat_at, null: false
+      t.bigint   :supervisor_id
+      t.integer  :pid,          null: false
+      t.string   :hostname
+      t.text     :metadata
+      t.datetime :created_at,   null: false
+      t.string   :name,         null: false
+      t.index [:last_heartbeat_at]
+      t.index [:name, :supervisor_id], unique: true
+      t.index [:supervisor_id]
+    end
+
+    create_table :solid_queue_recurring_tasks, force: :cascade do |t|
+      t.string   :key,          null: false
+      t.string   :schedule,     null: false
+      t.string   :command,      limit: 2048
+      t.string   :class_name
+      t.text     :arguments
+      t.string   :queue_name
+      t.integer  :priority,     default: 0
+      t.boolean  :static,       default: true, null: false
+      t.datetime :last_run_at
+      t.datetime :created_at,   null: false
+      t.datetime :updated_at,   null: false
+      t.index [:key], unique: true
+      t.index [:static]
+    end
+
+    create_table :solid_queue_recurring_executions, force: :cascade do |t|
+      t.bigint   :job_id,       null: false
+      t.string   :task_key,     null: false
+      t.datetime :run_at,       null: false
+      t.datetime :created_at,   null: false
+      t.index [:job_id], unique: true
+      t.index [:task_key, :run_at], unique: true
+    end
+
+    add_foreign_key :solid_queue_blocked_executions,   :solid_queue_jobs, column: :job_id, on_delete: :cascade
+    add_foreign_key :solid_queue_claimed_executions,   :solid_queue_jobs, column: :job_id, on_delete: :cascade
+    add_foreign_key :solid_queue_failed_executions,    :solid_queue_jobs, column: :job_id, on_delete: :cascade
+    add_foreign_key :solid_queue_ready_executions,     :solid_queue_jobs, column: :job_id, on_delete: :cascade
+    add_foreign_key :solid_queue_recurring_executions, :solid_queue_jobs, column: :job_id, on_delete: :cascade
+    add_foreign_key :solid_queue_scheduled_executions, :solid_queue_jobs, column: :job_id, on_delete: :cascade
+  end
+end
