e6c33f3f0f7a08d27ea4cddb861457bb75afba3b
Sumit Maurya <mauryasumit222@gmail.com>
2026-06-17 16:03:54 +0530

    Enable PWA feature for mobile installation

--- config/routes.rb
+++ config/routes.rb
@@ -160,6 +160,10 @@ Rails.application.routes.draw do
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   # ─── Saved Collections ──────────────────────────────────────────────────────
   resources :bookmark_collections, only: [:index, :create, :show, :update, :destroy] do
     member do
